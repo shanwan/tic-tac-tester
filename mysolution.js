78f2108dcf4df9461ba4ff43069df6758c862d97
ShanWan <shan.wan.loo@gmail.com>
2016-12-13 16:13:16 +0800

    for debugging

--- mysolution.js
+++ mysolution.js
@@ -1,36 +1,82 @@
-// var p1clicked = [0,1,4,2]
-// var p2clicked = []
-// var box = [0,1,2,3,4,5,6,7,8]
-// var gameOver = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 var gameOver = false
-// var whoWon = 0
-var usedIndexes = []
-
-
-//index is the selected box by the player
+// var grid = [0,0,0,
+//             0,0,0,
+//             0,0,0]
+var grid = [0, 0, 0,
+            0, 0, 0,
+            0, 0, 0]
+var numberofturns = 0
+var winner = 0
+// simulateGame('P1 Top Row Victory', [0, 3, 1, 4, 2], playerOne)
+// index refers to the position in the grid u want to go to
+// assuming player 1 starts first always
 function playTurn(index) {
-  // for (var i = 0; i < usedIndexes.length; i++) {
-    // if (INDEX i is in array MOVES, don't allow the move. return false)
-    if (usedIndexes.indexOf(index) === -1) { // if cannot find input
-      usedIndexes.push(index)
-    }
-      return true //allow the move
-    } else {
-    return false //dont allow the move
+  if(isGameOver () === true) {
+    return false
   }
-
-
-// console.log(playTurn(2));
-
-function isGameOver() {
-  return gameOver
+  else if (grid[index] === 1 || grid[index] === 2) {
+    return false
+  }
+  else if (numberofturns === 0 || numberofturns % 2 !== 0) {
+    grid[index] = 1
+    numberofturns += 1
+    return true
+  }
+  else if (numberofturns === 1 || numberofturns % 2 === 0) {
+    grid[index] = 2
+    numberofturns += 1
+    return true
+  }
 }
 
+console.log(playTurn(4));
+
+function isGameOver () {
+  if( whoWon() === 1 || whoWon() === 2 || whoWon() === 3) {
+    return true
+  } else {
+    return false
+  }
+}
+// simulateGame('P1 Top Row Victory', [0, 3, 1, 4, 2], playerOne)
 function whoWon() {
-  // if () player1wins
-  gameOver = true
+  function getSum(a,b) {
+    return a+b;
+  }
+// grid = [0,1,2,
+//         3,4,5,
+//         6,7,8]
+  if (grid.reduce(getSum) === 0) {
+    return 0
+  } else if ((grid[3] === grid[4]) && (grid[4] === grid[5])) {
+    winner = grid[3]
+    return winner
+  } else if ((grid[0] === grid[1]) && (grid[1] === grid[2])) {
+    winner = grid[0]
+    return winner
+  } else if ((grid[6] === grid[7]) && (grid[7] === grid[8])) {
+    winner = grid[6]
+    return winner
+  } else if ((grid[0] === grid[4]) && (grid[4] === grid[8])) {
+    winner = grid[0]
+    return winner
+  } else if ((grid[2] === grid[4]) && (grid[4] === grid[6])) {
+    winner = grid[2]
+    return winner
+  } else if ((grid[0] === grid[3]) && (grid[3] === grid[6])) {
+    winner = grid[0]
+    return winner
+  } else if ((grid[1] === grid[4]) && (grid[4] === grid[7])) {
+    winner = grid[1]
+    return winner
+  } else if ((grid[2] === grid[5]) && (grid[5] === grid[8])) {
+    winner = grid[2]
+    return winner
+  } else {
+  return 3
+  }
 }
 
-function restart() {
-
+function restart () {
+  grid.fill(0)
 }
